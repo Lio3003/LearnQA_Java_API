6fb835c15f872ba421cf75efdee28743cffa145e
Lio3003 <kuttubaeva3003@gmail.com>
2022-09-13 17:45:52 +0700

    HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
+
     public void testHelloWorld(){
-        System.out.println("Hello World");
+        String name = "Lilia";
+        System.out.println("Hello from " + name);
     }
 }
