cc37bca06603bdcec0e2b96f33e0050048cbf98b
Lio3003 <kuttubaeva3003@gmail.com>
2022-09-16 01:02:39 +0700

    Json parsing api/get_json_homework v.2

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -10,13 +10,9 @@ public class HelloWorldTest {
     @Test
 
     public void testRestAssured() {
-/*        Map<String, String> params = new HashMap<>();
-        params.put("message", null);
-        params.put("timestamp", "2021-06-04 16:41:51");*/
 
         JsonPath response = RestAssured
-/*                .given()
-                .queryParams(params)*/
+
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
         String message = response.get("messages.message[1]");
@@ -25,17 +21,5 @@ public class HelloWorldTest {
         String timestamp = response.get("messages.timestamp[1]");
         System.out.println(timestamp);
 
-/*        if (message == null) {
-            System.out.println("The key 'messages.message[1]' is absent");
-        } else {
-            System.out.println(message);
-
-        }
-        if (timestamp == null) {
-            System.out.println("The key 'messages.timestamp[1]' is absent");
-        } else {
-            System.out.println(timestamp);
-
-        }*/
     }
 }
