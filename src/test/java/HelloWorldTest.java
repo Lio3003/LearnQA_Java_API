1a77099e005cbfa0bc1d44d1a2181d1f02344d5d
Lio3003 <kuttubaeva3003@gmail.com>
2022-09-18 19:23:45 +0700

    Ex6*.Redirect

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,24 +2,18 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 
-//import java.util.HashMap;
-//import java.util.Map;
-
 
 public class HelloWorldTest {
     @Test
 
     public void testRestAssured() {
-        //Map<String, String> headers = new HashMap<>();
-        //headers.put("MyHeader1", "MyValue1");
-        //headers.put("MyHeader2", "MyValue2");
 
         Response response = RestAssured
                 .given()
                 .redirects()
                 .follow(false)
                 .when()
-                .get("https://playground.learnqa.ru/api/get_303")
+                .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
         response.prettyPrint();
 
