2a3195b214a4cf073091430f9043701fe09ae16d
Lio3003 <kuttubaeva3003@gmail.com>
2022-09-18 19:19:20 +0700

    8. Headers (location)

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,21 +2,29 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 
+//import java.util.HashMap;
+//import java.util.Map;
+
 
 public class HelloWorldTest {
     @Test
 
     public void testRestAssured() {
+        //Map<String, String> headers = new HashMap<>();
+        //headers.put("MyHeader1", "MyValue1");
+        //headers.put("MyHeader2", "MyValue2");
 
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
                 .when()
                 .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
 
     }
 }
