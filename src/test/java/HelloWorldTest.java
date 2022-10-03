a86f5586d7df7b401c61fb30cef4c8531c86163a
Lio3003 <kuttubaeva3003@gmail.com>
2022-10-03 14:07:26 +0700

    Ex.10 Short phrase test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,35 +1,25 @@
-import io.restassured.path.json.JsonPath;
-import io.restassured.RestAssured;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.util.HashMap;
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class HelloWorldTest {
-    @ParameterizedTest
-    @ValueSource(strings = {"", "John", "Pete"})
 
-    public void testHelloMethodWithoutName(String name) {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "Compare actual query type with the type from the parameter", "Compare actual1","Compare actual"})
+
+    public void testTextLength(String text) {
         Map<String, String> queryParams = new HashMap<>();
 
-        if(name.length() > 0)
+        if(text.length() >= 15)
         {
-            queryParams.put("name", name);
+            queryParams.put("text", text);
         }
 
-        JsonPath response = RestAssured
-                .given()
-                .queryParams(queryParams)
-                .get("https://playground.learnqa.ru/api/hello")
-                .jsonPath();
-            String answer = response.getString("answer");
-            String expectedName = (name.length() > 0) ? name: "someone";
-            assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
-
-
+        assertEquals(text.length() >= 15, true, "The length of text more than 15 symbols!");
     }
 
-        }
+
+}
 
