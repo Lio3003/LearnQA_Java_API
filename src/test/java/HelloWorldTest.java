68eae79ed77623dc00953b049049e5835c24a380
Lio3003 <kuttubaeva3003@gmail.com>
2022-10-03 12:01:13 +0700

    3.2 Parameterized Test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,31 +1,32 @@
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
+import io.restassured.path.json.JsonPath;
 import io.restassured.RestAssured;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.HashMap;
+import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class HelloWorldTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
 
-    public void testFor200() {
+    public void testHelloMethodWithoutName(String name) {
+        Map<String, String> queryParams = new HashMap<>();
 
-            Response response = RestAssured
-                    .get("https://playground.learnqa.ru/api/map")
-                    .andReturn();
-          //  assertTrue(response.statusCode() == 200, "Unexpected status code");
-            assertEquals(200, response.statusCode(), "Unexpected status code");
+        if(name.length() > 0)
+        {
+            queryParams.put("name", name);
+        }
 
-
-    }
-    @Test
-
-    public void testFor404() {
-
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/map1")
-                .andReturn();
-        //  assertTrue(response.statusCode() == 200, "Unexpected status code");
-        assertEquals(404, response.statusCode(), "Unexpected status code");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+            String answer = response.getString("answer");
+            String expectedName = (name.length() > 0) ? name: "someone";
+            assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
 
 
     }
